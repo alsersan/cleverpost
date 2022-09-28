e3e9c3cdfacc95d822e474b1ec21e989c45de489
Álvaro Serrano <alsersan20@gmail.com>
2022-09-28 23:14:21 +0200

    Add posts actions

---
+++ src/redux/actions/postsActions.ts
@@ -0,0 +1,23 @@
+import { Post } from 'models';
+import { actionTypes } from '../actionTypes';
+
+interface GetPostsAction {
+  type: actionTypes.GET_POSTS;
+}
+
+interface GetPostsSuccessAction {
+  type: actionTypes.GET_POSTS_SUCCESS;
+  payload: Post[];
+}
+
+interface GetPostsErrorAction {
+  type: actionTypes.GET_POSTS_ERROR;
+  payload: string;
+}
+
+export type GetPostsActions =
+  | GetPostsAction
+  | GetPostsSuccessAction
+  | GetPostsErrorAction;
+
+export type PostActions = GetPostsActions;
