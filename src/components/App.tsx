afe41537e982cd40af7692ed62f5ca67dda92fba
Álvaro Serrano <alsersan20@gmail.com>
2022-09-29 00:02:01 +0200

    Add redux store and provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>CleverPost</h1>;
-}
+import { Provider } from 'react-redux';
+import { store } from 'state';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <h1>CleverPost</h1>
+    </Provider>
+  );
+};
 
 export default App;
