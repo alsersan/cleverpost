3aade8d409764789c0a89c160515f3988d2881e4
Álvaro Serrano <alsersan20@gmail.com>
2022-09-29 00:17:04 +0200

    Create AppThunk type

---
+++ src/state/actionCreators/appThunk.ts
@@ -0,0 +1,8 @@
+import { Action } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from '../store';
+
+export type AppThunk<
+  ActionType extends Action,
+  ReturnType = void
+> = ThunkAction<ReturnType, RootState, unknown, ActionType>;
