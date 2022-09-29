636bea619ebc19dc0e3e534b7a1c7f2046cb5d3e
Álvaro Serrano <alsersan20@gmail.com>
2022-09-30 00:08:16 +0200

    Recover posts and users on app first load

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useActions } from 'hooks/useActions';
+
 const App = () => {
+  const { getPostsWithUsers } = useActions();
+
+  useEffect(() => {
+    getPostsWithUsers();
+  }, []);
+
   return <h1>CleverPost</h1>;
 };
 
