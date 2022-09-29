59189ff21f7a7973ff09cdabb306d9f16c3872ec
Álvaro Serrano <alsersan20@gmail.com>
2022-09-29 09:42:48 +0200

    Create users action creators

---
+++ src/state/actionCreators/usersActionCreators.ts
@@ -0,0 +1,27 @@
+import { getAllUsers } from 'services/users';
+import { UsersActions } from '../actions/usersActions';
+import { usersActionTypes } from '../actionTypes';
+import { AppThunk } from './appThunk';
+
+export const getUsers = (): AppThunk<UsersActions> => {
+  return async (dispatch, getState) => {
+    const usersStore = getState().users.data;
+    if (usersStore.length) return;
+
+    dispatch({ type: usersActionTypes.GET_USERS });
+    try {
+      const data = await getAllUsers();
+      dispatch({
+        type: usersActionTypes.GET_USERS_SUCCESS,
+        payload: data
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: usersActionTypes.GET_USERS_ERROR,
+          payload: err.message
+        });
+      }
+    }
+  };
+};
