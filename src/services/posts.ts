e422f21e382936fab3cd602f171777499bc2fb8d
Álvaro Serrano <alsersan20@gmail.com>
2022-09-29 23:13:42 +0200

    Add processPosts function to postsServices

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,7 +1,20 @@
-import { ApiPost } from 'models';
+import { ApiPost, Post, User } from 'models';
 
 const baseURL = 'http://jsonplaceholder.typicode.com';
 
 export const getAllPosts = (): Promise<ApiPost[]> => {
   return fetch(`${baseURL}/posts`).then((res) => res.json());
+};
+
+export const processPosts = (posts: ApiPost[], users: User[]): Post[] => {
+  const usersMapping: {
+    [key: number]: { username: string; userAvatar: string };
+  } = {};
+  users.forEach((user) => {
+    usersMapping[user.id] = {
+      username: user.username,
+      userAvatar: user.userAvatar
+    };
+  });
+  return posts.map((post) => ({ ...post, ...usersMapping[post.userId] }));
 };
