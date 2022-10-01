d8b5595a5fd2eac0a6d5cc0e5e725173cba3388b
Álvaro Serrano <alsersan20@gmail.com>
2022-10-02 00:50:39 +0200

    Add delete and edit icons to card post

--- src/components/CardPost/CardPost.tsx
+++ src/components/CardPost/CardPost.tsx
@@ -1,4 +1,6 @@
 import { Post } from 'models';
+import deleteIcon from 'assets/icons/delete.svg';
+import editIcon from 'assets/icons/edit.svg';
 import './CardPost.scss';
 
 interface Props {
@@ -17,6 +19,25 @@ export const CardPost: React.FC<Props> = ({ postData }) => {
         <span className="post__username">{postData.username}</span>
       </div>
       <div className="post__content">
+        <div className="post__flex-wrapper">
+          <span className="post__id"># {postData.id}</span>
+          <div className="post__icons-container">
+            <button>
+              <img
+                className="post__icon"
+                src={editIcon}
+                alt="Down arrow icon"
+              />
+            </button>
+            <button>
+              <img
+                className="post__icon"
+                src={deleteIcon}
+                alt="Down arrow icon"
+              />
+            </button>
+          </div>
+        </div>
         <h3 className="post__title">{postData.title}</h3>
         <p className="post__body">{postData.body}</p>
       </div>
