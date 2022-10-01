12de6fa9035f65350895e8d413548bd84cac8229
Álvaro Serrano <alsersan20@gmail.com>
2022-10-01 18:59:31 +0200

    Add user section to header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,10 +1,22 @@
 import './Header.scss';
+import arrowDownIcon from 'assets/icons/arrow-down.svg';
 
 export const Header = () => {
   return (
     <nav className="header">
-      <img className="header__logo" src="" alt="Cleverpost logo" />
-      <span>User placeholder</span>
+      <span>CleverPost</span>
+      <div className="logued-user">
+        <div className="logued-user__name-wrapper">
+          <p className="logued-user__name">John Doe</p>
+          <p className="logued-user__role">Administrator</p>
+        </div>
+        <div className="logued-user__avatar" />
+        <img
+          className="logues-user__arrow-icon"
+          src={arrowDownIcon}
+          alt="Down arrow icon"
+        />
+      </div>
     </nav>
   );
 };
