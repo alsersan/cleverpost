21dd77e4f9941136c74768041634f6ed36aa957c
Álvaro Serrano <alsersan20@gmail.com>
2022-10-02 12:14:48 +0200

    Add sidebar nav

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,30 @@
 import './Sidebar.scss';
+import usersLogo from 'assets/icons/users.svg';
+import postLogo from 'assets/icons/post.svg';
 
 export const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <nav className="sidebar-nav">
+        <ul className="sidebar-nav__section-list">
+          <li className="sidebar-nav__section">
+            <a href="" className="sidebar-nav__link">
+              <div className="sidebar-nav__icon-wrapper">
+                <img src={postLogo} alt="Post icon" />
+              </div>
+              Posts
+            </a>
+          </li>
+          <li className="sidebar-nav__section">
+            <a href="" className="sidebar-nav__link">
+              <div className="sidebar-nav__icon-wrapper">
+                <img src={usersLogo} alt="Users icon" />
+              </div>
+              Users
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
