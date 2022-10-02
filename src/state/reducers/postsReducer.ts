5af08313996383b8376967263daa553dc5f4cd85
Álvaro Serrano <alsersan20@gmail.com>
2022-10-02 20:37:15 +0200

    Update postsReducer with update and delete cases

--- src/state/reducers/postsReducer.ts
+++ src/state/reducers/postsReducer.ts
@@ -14,12 +14,43 @@ export const postsReducer = (
   action: PostsActions
 ): postsState => {
   switch (action.type) {
+    // GET POSTS
     case postsActionTypes.GET_POSTS:
       return { loading: true, error: null, data: [] };
     case postsActionTypes.GET_POSTS_SUCCESS:
       return { loading: false, error: null, data: action.payload };
     case postsActionTypes.GET_POSTS_ERROR:
       return { loading: false, error: action.payload, data: [] };
+
+    // UPDATE POST
+    case postsActionTypes.EDIT_POST:
+      // No loading. Switch to true if necessary
+      return { loading: false, error: null, data: state.data };
+    case postsActionTypes.EDIT_POST_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: state.data.map((el) =>
+          el.id === action.payload.id ? action.payload : el
+        )
+      };
+    case postsActionTypes.EDIT_POST_ERROR:
+      return { loading: false, error: action.payload, data: state.data };
+
+    // DELETE POST
+    case postsActionTypes.DELETE_POST:
+      // No loading. Switch to true if necessary
+      return { loading: false, error: null, data: state.data };
+    case postsActionTypes.DELETE_POST_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: state.data.filter((el) => el.id !== action.payload)
+      };
+    case postsActionTypes.DELETE_POST_ERROR:
+      return { loading: false, error: action.payload, data: state.data };
+
+    // FALLBACK
     default:
       return state;
   }
