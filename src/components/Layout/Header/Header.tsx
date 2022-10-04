b6ecb64bf428fd7447b50f5943cd124f90da9eb0
Álvaro Serrano <alsersan20@gmail.com>
2022-10-04 02:10:41 +0200

    Add header dropdown menu and translations

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,8 +1,9 @@
 import './Header.scss';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Link } from 'react-router-dom';
 
 import arrowDownIcon from 'assets/icons/arrow-down.svg';
+import logoutIcon from 'assets/icons/logout.svg';
 
 export const Header = () => {
   const intl = useIntl();
@@ -12,17 +13,35 @@ export const Header = () => {
       <Link to="/" className="header__logo">
         CleverPost
       </Link>
-      <div className="logued-user">
-        <div className="logued-user__name-wrapper">
-          <p className="logued-user__name">John Doe</p>
-          <p className="logued-user__role">Administrator</p>
+      <div className="header__menu-wrapper">
+        <div className="logued-user">
+          <div className="logued-user__name-wrapper">
+            <p className="logued-user__name">John Doe</p>
+            <p className="logued-user__role">
+              <FormattedMessage id="header.user-role" />
+            </p>
+          </div>
+          <div className="logued-user__avatar" />
+          <img
+            className="logued-user__arrow-icon"
+            src={arrowDownIcon}
+            alt={intl.formatMessage({ id: 'icon.down-arrow-alt' })}
+          />
         </div>
-        <div className="logued-user__avatar" />
-        <img
-          className="logued-user__arrow-icon"
-          src={arrowDownIcon}
-          alt={intl.formatMessage({ id: 'icon.down-arrow-alt' })}
-        />
+        <ul className="dropdown-menu">
+          <li>
+            <button className="dropdown-menu__button">
+              <div className="dropdown-menu__icon-wrapper">
+                <img
+                  className="dropdown-menu__icon"
+                  src={logoutIcon}
+                  alt={intl.formatMessage({ id: 'icon.logout-alt' })}
+                />
+              </div>
+              <FormattedMessage id="header.logout" />
+            </button>
+          </li>
+        </ul>
       </div>
     </nav>
   );
