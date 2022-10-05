739ac9f530c7f982ab221c959a2975344d76a44d
Álvaro Serrano <alsersan20@gmail.com>
2022-10-05 02:26:28 +0200

    Add tests for UsersPage component

---
+++ src/pages/__tests__/UsersPage.test.tsx
@@ -0,0 +1,67 @@
+import { screen } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter } from 'react-router-dom';
+
+import { messages } from 'lang/languages';
+import { User } from 'models';
+import { RootState, postsState } from 'state';
+import { renderStoreProvider } from 'tests/test-utils';
+
+import { UsersPage } from '../UsersPage';
+
+const mockState: RootState = {
+  users: {
+    error: null,
+    loading: false,
+    data: [
+      { username: 'John', id: 1 },
+      { username: 'Patrick', id: 2 },
+      { username: 'Sarah', id: 3 }
+    ] as User[]
+  },
+  posts: {} as postsState
+};
+
+describe('UsersPage Component', () => {
+  describe('When the component is instantiated', () => {
+    beforeEach(() => {
+      renderStoreProvider(
+        <BrowserRouter>
+          <IntlProvider locale={'en-US'} messages={messages['en']}>
+            <UsersPage />
+          </IntlProvider>
+        </BrowserRouter>,
+        { preloadedState: mockState }
+      );
+    });
+
+    test('renders header', () => {
+      expect(screen.getByText(/users page/i)).toBeInTheDocument();
+    });
+
+    test('renders users', () => {
+      expect(screen.getByText(/john/i)).toBeInTheDocument();
+    });
+
+    test('renders list of users', () => {
+      expect(screen.getAllByRole('listitem')).toHaveLength(3);
+    });
+  });
+
+  describe('When locale changes', () => {
+    beforeEach(() => {
+      renderStoreProvider(
+        <BrowserRouter>
+          <IntlProvider locale={'es-ES'} messages={messages['es']}>
+            <UsersPage />
+          </IntlProvider>
+        </BrowserRouter>,
+        { preloadedState: mockState }
+      );
+    });
+
+    test('renders the correct message', () => {
+      expect(screen.getByText(/página de usuarios/i)).toBeInTheDocument();
+    });
+  });
+});
