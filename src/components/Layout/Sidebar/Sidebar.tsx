1d938e829ca76352198656db1153fa084e606572
Álvaro Serrano <alsersan20@gmail.com>
2022-10-06 01:10:35 +0200

    Hide sidebar if not logued in

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import './Sidebar.scss';
+import { useAuth0 } from '@auth0/auth0-react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { Link } from 'react-router-dom';
 
@@ -7,36 +8,41 @@ import usersLogo from 'assets/icons/users.svg';
 
 export const Sidebar = () => {
   const intl = useIntl();
+  const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="sidebar">
-      <nav className="sidebar-nav">
-        <ul className="sidebar-nav__section-list">
-          <li className="sidebar-nav__section">
-            <Link to="/posts" className="sidebar-nav__link">
-              <div className="sidebar-nav__icon-wrapper">
-                <img
-                  src={postLogo}
-                  alt={intl.formatMessage({ id: 'icon.post-alt' })}
-                />
-              </div>
-              <FormattedMessage id="sidebar.posts" />
-            </Link>
-          </li>
-          <li className="sidebar-nav__section">
-            <Link to="/users" className="sidebar-nav__link">
-              <div className="sidebar-nav__icon-wrapper">
-                <img
-                  src={usersLogo}
-                  alt={intl.formatMessage({ id: 'icon.users-alt' })}
-                />
-              </div>
-              <FormattedMessage id="sidebar.users" />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <small className="sidebar__footer">&copy; Álvaro Serrano 2022</small>
-    </div>
+    <>
+      {isAuthenticated && (
+        <div className="sidebar">
+          <nav className="sidebar-nav">
+            <ul className="sidebar-nav__section-list">
+              <li className="sidebar-nav__section">
+                <Link to="/posts" className="sidebar-nav__link">
+                  <div className="sidebar-nav__icon-wrapper">
+                    <img
+                      src={postLogo}
+                      alt={intl.formatMessage({ id: 'icon.post-alt' })}
+                    />
+                  </div>
+                  <FormattedMessage id="sidebar.posts" />
+                </Link>
+              </li>
+              <li className="sidebar-nav__section">
+                <Link to="/users" className="sidebar-nav__link">
+                  <div className="sidebar-nav__icon-wrapper">
+                    <img
+                      src={usersLogo}
+                      alt={intl.formatMessage({ id: 'icon.users-alt' })}
+                    />
+                  </div>
+                  <FormattedMessage id="sidebar.users" />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <small className="sidebar__footer">&copy; Álvaro Serrano 2022</small>
+        </div>
+      )}
+    </>
   );
 };
