b4f22bd0ca0a11abf4dba887071635cc77029864
Álvaro Serrano <alsersan20@gmail.com>
2022-10-06 01:50:54 +0200

    Add not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,28 @@
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Link } from 'react-router-dom';
 
+import './page.scss';
+import pageNotFoundImg from 'assets/img/page-not-found.png';
+
 export const NotFoundPage = () => {
+  const intl = useIntl();
+
   return (
-    <div className="page">
-      <h2>Page not found</h2>
-      <p>We're sorry, the page you requested could not be found.</p>
-      <Link to="/">Go to homepage</Link>
+    <div className="page page--content-centered">
+      <h2>
+        <FormattedMessage id="not-found-page.title" />
+      </h2>
+      <img
+        src={pageNotFoundImg}
+        alt={intl.formatMessage({ id: 'img.not-found' })}
+        className="not-found-img"
+      />
+      <p>
+        <FormattedMessage id="not-found-page.text" />
+      </p>
+      <Link to="/" className="go-back-link">
+        <FormattedMessage id="not-found-page.link" />
+      </Link>
     </div>
   );
 };
