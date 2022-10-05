8bacfd7672dad88de4d43726436b4458abb4e0c5
Álvaro Serrano <alsersan20@gmail.com>
2022-10-05 11:41:28 +0200

    Implement login

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -10,12 +11,17 @@ import { store } from 'state';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <LangSwitcherProvider>
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </LangSwitcherProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+      redirectUri={window.location.origin}>
+      <LangSwitcherProvider>
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+      </LangSwitcherProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
