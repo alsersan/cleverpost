9dd70a251ac754211eb9a6bc46093b3982ea9620
Álvaro Serrano <alsersan20@gmail.com>
2022-10-06 02:35:26 +0200

    Move type definitions to the correct file

--- src/models/globals/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test';
-    PUBLIC_URL: string;
-    REACT_APP_AUTH_DOMAIN: string;
-    REACT_APP_AUTH_CLIENT_ID: string;
-  }
-}
