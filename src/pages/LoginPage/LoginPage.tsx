0a0ce6f5b2742dc9dc991b71b3587451d4a26e20
Álvaro Serrano <alsersan20@gmail.com>
2023-10-13 15:33:25 +0200

    Fix login layout

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -10,21 +10,23 @@ export const LoginPage = () => {
 
   return (
     <div className="login-page">
-      <img
-        src={companyLogo}
-        alt={intl.formatMessage({ id: 'img.company-logo-alt' })}
-        className="login-page__image"
-      />
-      <h2 className="login-page__title">
-        <FormattedMessage id="login-page.title" />
-      </h2>
-      <span>
-        {' '}
-        <FormattedMessage id="login-page.text" />
-      </span>
-      <button onClick={() => loginWithRedirect()} className="login-page__btn">
-        <FormattedMessage id="login-page.btn" />
-      </button>
+      <div className="login-page__content-wrapper">
+        <img
+          src={companyLogo}
+          alt={intl.formatMessage({ id: 'img.company-logo-alt' })}
+          className="login-page__image"
+        />
+        <h2 className="login-page__title">
+          <FormattedMessage id="login-page.title" />
+        </h2>
+        <span>
+          {' '}
+          <FormattedMessage id="login-page.text" />
+        </span>
+        <button onClick={() => loginWithRedirect()} className="login-page__btn">
+          <FormattedMessage id="login-page.btn" />
+        </button>
+      </div>
       <div className="login-page__square login-page__square--left"></div>
       <div className="login-page__square login-page__square--right"></div>
     </div>
